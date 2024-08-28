2dcb141e5bdaec048c9f98439b1880654bb49b64
Carolay Del Rosario Gómez Borja <carolay@Carolays-MacBook-Pro-2.local>
2024-08-27 23:39:14 -0500

    acción de encriptar

--- js/encriptador.js
+++ js/encriptador.js
@@ -10,8 +10,8 @@ btnEncriptar.addEventListener("clik", e=>{
     console.log(txt);
 
     if(texto == "") {
-        aviso.style.background = "#0a3871";
-        aviso.style.color = "#fff";
+        aviso.style.background = "#0A3871";
+        aviso.style.color = "#FFFF";
         aviso.style.fontWeight = "800";
         aviso.textContent = "El campo de texto no debe estar vacío";
 
@@ -20,8 +20,30 @@ btnEncriptar.addEventListener("clik", e=>{
         },1500);
     }
          
+   else if (texto !== txt) {
+        aviso.style.background = "#0A3871";
+        aviso.style.color = "#FFFF";
+        aviso.style.fontWeight = "800";
+        aviso.textContent = "No debe tener acentos ni caracteres especiales";
 
+        setTimeout(() => {
+            aviso.removeAttribute("style");
+        },1500);
+
+   }
    
+
+    else if (texto !== texto.tolowerCase()) {
+        aviso.style.background = "#0A3871";
+        aviso.style.color = "#FFFF";
+        aviso.style.fontWeight = "800";
+        aviso.textContent = "El texto debe ser todo en minúscula";
+
+        setTimeout(() => {
+            aviso.removeAttribute("style");
+        },1500);
+   }
+    
 
 })
 
